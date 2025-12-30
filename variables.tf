c5525d810a1ca8f8907aa85cb8670ae23ce82f09
Ishikawa Ryuto <noreply@rishikawa.net>
2025-12-31 03:06:56 +0900

    root_passを外部から渡す

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "linode_pat" {
   type      = string
   sensitive = true
 }
+
+variable "root_pass" {
+  type        = string
+  sensitive   = true
+  description = "Default root password for instances"
+}
